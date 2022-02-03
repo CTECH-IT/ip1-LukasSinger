2fa9c0ab923f61ae8ed4fc9f784c2e85d2ecc019
Lukas Singer <7627101@isd535.org>
2022-02-03 11:32:27 -0600

    Set up Phaser

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
-// Put your JavaScript here
+// Initialize the game
+const game = new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+});
+
+function preload() {}
+
+function create() {}
+
+function update() {}
