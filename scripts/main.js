73c71b8df706325766aebdb32645e622ca87b90c
Lukas Singer <7627101@isd535.org>
2022-02-08 11:15:13 -0600

    Basic tilemap

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,36 @@
-// Initialize the game
-const game = new Phaser.Game({
-  type: Phaser.AUTO,
-  width: 1280,
-  height: 720,
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: false,
+let level = 1;
+
+let levels;
+let game;
+
+load();
+
+async function load() {
+  await fetch("data/levels.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      levels = data;
+      return levels;
+    });
+  game = new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    physics: {
+      default: "arcade",
+      arcade: {
+        debug: false,
+      },
     },
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
-});
+    scene: {
+      preload: preload,
+      create: create,
+      update: update,
+    },
+  });
+}
 
 function preload() {
   this.load.image("sky", "assets/sky.png");
@@ -25,15 +41,16 @@ function preload() {
   });
 }
 
-function create() {
+async function create() {
   this.add.image(1280 / 2, 720 / 2, "sky");
   const map = this.make.tilemap({
-    data: [],
-    tileWidth: 64,
-    tileHeight: 64,
+    data: levels[level],
+    tileWidth: 32,
+    tileHeight: 32,
   });
   map.addTilesetImage("tileset");
-  const layer = map.createLayer(0, "tileset", 0, 0);
+  const layer = map.createLayer(0, "tileset", 1280 / 2 - 720 / 2, 0);
+  layer.scale = 720 / 320;
 }
 
 function update() {}
